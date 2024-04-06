b0b13ff5e4cdc709a2bf44401c4d6f1f004cf8c6
garick-git <martin.mendez@erpchile.com>
2024-04-06 16:22:03 -0500

    created index.css.

--- index.php
+++ index.php
@@ -1,83 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Computer Security Homework 4</title>
-
-    <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-        }
-
-        .form-container{
-            width: 25%;
-        }
-        
-        form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-shadow: 0 0 30px rgba(0, 0, 0, 0.1);
-            width: full;
-        }
-
-        input {
-            margin: 10px;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            width: 200px;
-            transition: transform 500ms;
-            width: 70%;
-        }
-
-        .enabled-button{
-            margin: 10px;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            width: 200px;
-            transition: transform 500ms;
-            width: 70%;
-            cursor: pointer;
-        }
-
-        .enabled-button:hover{
-            transform: scale(1.03);
-            background-color: lightskyblue;
-            color: black;
-        }
-
-        .disabled-button{
-            margin: 10px;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            width: 200px;
-            width: 70%;
-            background-color: lightgray;
-            color: black;
-        }
-
-        input:hover{
-            transform: scale(1.03);
-        }
-    </style>
-
 </head>
-<body>
+<body class="index-body">
 
     <h1>Log in</h1>
     <div class="form-container">
